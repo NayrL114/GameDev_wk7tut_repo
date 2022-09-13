a5d75a5dec157bf50871dbe834f759bc89c3211a
NayrL <40843838+NayrL114@users.noreply.github.com>
2022-09-14 00:08:09 +1000

    80 percent complete

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -34,23 +34,31 @@ public class InputManager : MonoBehaviour
         // Inputs
         if (Input.GetKeyDown(KeyCode.A))
         {
+            Debug.Log("Moving Left");
             tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(-2.0f, 0.5f, 0.0f), 1.5f);
             keyText.text = "A";
+            keyTime = 0;
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
+            Debug.Log("Moving Right");
             tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(2.0f, 0.5f, 0.0f), 1.5f);
             keyText.text = "D";
+            keyTime = 0;
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
+            Debug.Log("Moving Down");
             tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(0.0f, 0.5f, -2.0f), 1.5f);
             keyText.text = "S";
+            keyTime = 0;
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
+            Debug.Log("Moving Up");
             tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(0.0f, 0.5f, 2.0f), 1.5f);
-            keyText.text = "W";
+            keyText.text = "W"; 
+            keyTime = 0;
         }
         //AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
 
--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -6,6 +6,7 @@ public class Tweener : MonoBehaviour
 {
 
     private Tween activeTween;
+    private float timer;
     
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,31 @@ public class Tweener : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        if (activeTween != null)
+        {
+            timer += Time.deltaTime;
+            if (Vector3.Distance(activeTween.Target.position, activeTween.EndPos) > 0.1f)
+            {
+                //Debug.Log("First Condition");
+                activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timer / activeTween.Duration);
+                // Relative distance will be (current distance away from starting point ) / (total distance)
+                /*
+                activeTween.Target.position = Vector3.Lerp(
+                    activeTween.StartPos,
+                    activeTween.EndPos,
+                    Vector3.Distance(activeTween.Target.position, activeTween.StartPos) / Vector3.Distance(activeTween.StartPos, activeTween.EndPos)
+                    );
+                */
+            }
+            else if (Vector3.Distance(activeTween.Target.position, activeTween.EndPos) <= 0.1f)
+            {
+                //Debug.Log("Second Condition");
+                activeTween.Target.position = activeTween.EndPos;
+                activeTween = null;
+                timer = 0;
+            }
+        }
         
     }
 
