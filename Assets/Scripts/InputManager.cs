21d0bd85d8638fe2ec5083a1446c8d97ef481b53
NayrL <40843838+NayrL114@users.noreply.github.com>
2022-09-14 23:14:15 +1000

    100 percent partial done

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -10,12 +10,15 @@ public class InputManager : MonoBehaviour
     private Tweener tweener;
     public Text keyText;
     private int keyTime;
+    private List<GameObject> itemList;
 
     //private Transform itemTrans;
     
     // Start is called before the first frame update
     void Start()
     {
+        itemList = new List<GameObject>();
+        itemList.Add(item);
         tweener = GetComponent<Tweener>();
     }
 
@@ -35,33 +38,60 @@ public class InputManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("Moving Left");
-            tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(-2.0f, 0.5f, 0.0f), 1.5f);
+            //tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(-2.0f, 0.5f, 0.0f), 1.5f);
+            addTweener(new Vector3(-2.0f, 0.5f, 0.0f), 1.5f);
             keyText.text = "A";
             keyTime = 0;
         }
         else if (Input.GetKeyDown(KeyCode.D))
         {
             Debug.Log("Moving Right");
-            tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(2.0f, 0.5f, 0.0f), 1.5f);
+            //tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(2.0f, 0.5f, 0.0f), 1.5f);
+            addTweener(new Vector3(2.0f, 0.5f, 0.0f), 1.5f);
             keyText.text = "D";
             keyTime = 0;
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
             Debug.Log("Moving Down");
-            tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(0.0f, 0.5f, -2.0f), 1.5f);
+            //tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(0.0f, 0.5f, -2.0f), 1.5f);
+            addTweener(new Vector3(0.0f, 0.5f, -2.0f), 1.5f);
             keyText.text = "S";
             keyTime = 0;
         }
         else if (Input.GetKeyDown(KeyCode.W))
         {
             Debug.Log("Moving Up");
-            tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(0.0f, 0.5f, 2.0f), 1.5f);
+            //tweener.AddTween(item.GetComponent<Transform>(), item.GetComponent<Transform>().position, new Vector3(0.0f, 0.5f, 2.0f), 1.5f);
+            addTweener(new Vector3(0.0f, 0.5f, 2.0f), 1.5f);
             keyText.text = "W"; 
+            keyTime = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject itemClone = Instantiate(item, new Vector3(0.0f, 0.5f, 0.0f), item.transform.rotation);
+            itemList.Add(itemClone);
+            keyText.text = "Space";
             keyTime = 0;
         }
         //AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
 
+    }
+
+    private void addTweener(Vector3 endPos, float duration)
+    {
+        for (int i = 0; i < itemList.Count; i++)
+        {
+            if (tweener.AddTween(itemList[i].GetComponent<Transform>(), itemList[i].GetComponent<Transform>().position, endPos, duration))// if AddTween returns true
+            {
+                Debug.Log("supposely a tween should be added successfully");
+                i = itemList.Count;
+                //break;
+            }
+            Debug.Log(i + " and itemList count is " + itemList.Count);
+            // else, do nothing if AddTween returns false
+        }
+                        
     }
 
 
--- Assets/Scripts/Tweener.cs
+++ Assets/Scripts/Tweener.cs
@@ -5,55 +5,103 @@ using UnityEngine;
 public class Tweener : MonoBehaviour
 {
 
-    private Tween activeTween;
-    private float timer;
+    //private Tween activeTween;
+    private List<Tween> activeTweens;
+    //private float timer;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        activeTweens = new List<Tween>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (activeTween != null)
+        //Debug.Log(activeTweens.Count);
+        //if (activeTween != null)
+        if (activeTweens.Count > 0)
         {
-            timer += Time.deltaTime;
-            if (Vector3.Distance(activeTween.Target.position, activeTween.EndPos) > 0.1f)
+            foreach (Tween t in activeTweens)
             {
-                //Debug.Log("First Condition");
-                //activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timer / activeTween.Duration);
-                // Relative distance will be (current distance away from starting point ) / (total distance)
+                //timer += Time.deltaTime;
+                if (Vector3.Distance(t.Target.position, t.EndPos) > 0.1f)
+                {
+                    //Debug.Log("First Condition");
+                    //activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, timer / activeTween.Duration);
+                    // Relative distance will be (current distance away from starting point ) / (total distance)
 
-                float fractionTime = timer / activeTween.Duration;
-                activeTween.Target.position = Vector3.Lerp(activeTween.StartPos, activeTween.EndPos, fractionTime * fractionTime * fractionTime);
-                // Cubic easing in: function (t,b,c,d) t /= d; return c*t*t*t + b;
-                // t = current time; b = start value; c = change in value; d = duration;
+                    //float fractionTime = timer / t.Duration;
+                    float fractionTime = (Time.time - t.StartTime) / t.Duration;
+                    t.Target.position = Vector3.Lerp(t.StartPos, t.EndPos, fractionTime * fractionTime * fractionTime);
+                    // Cubic easing in: function (t,b,c,d) t /= d; return c*t*t*t + b;
+                    // t = current time; b = start value; c = change in value; d = duration;
 
-                /*
-                activeTween.Target.position = Vector3.Lerp(
-                    activeTween.StartPos,
-                    activeTween.EndPos,
-                    Vector3.Distance(activeTween.Target.position, activeTween.StartPos) / Vector3.Distance(activeTween.StartPos, activeTween.EndPos)
-                    );
-                */
+                    /*
+                    activeTween.Target.position = Vector3.Lerp(
+                        activeTween.StartPos,
+                        activeTween.EndPos,
+                        Vector3.Distance(activeTween.Target.position, activeTween.StartPos) / Vector3.Distance(activeTween.StartPos, activeTween.EndPos)
+                        );
+                    */
+                }
+                else if (Vector3.Distance(t.Target.position, t.EndPos) <= 0.1f)
+                {
+                    //Debug.Log("Second Condition");
+                    t.Target.position = t.EndPos;
+                    //activeTween = null;
+                    activeTweens.Remove(t);
+                    //timer = 0;
+                }
             }
-            else if (Vector3.Distance(activeTween.Target.position, activeTween.EndPos) <= 0.1f)
-            {
-                //Debug.Log("Second Condition");
-                activeTween.Target.position = activeTween.EndPos;
-                activeTween = null;
-                timer = 0;
-            }
+            
         }
         
     }
 
+    /*
     public void AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
     {
         if (activeTween == null) activeTween = new Tween(targetObject, startPos, endPos, Time.time, duration);
+    }
+    */
+
+    public bool AddTween(Transform targetObject, Vector3 startPos, Vector3 endPos, float duration)
+    {
+        Debug.Log("inside AddTween()");
+        if (activeTweens.Count == 0)
+        {
+            Debug.Log("list if empty, adding object");
+            activeTweens.Add(new Tween(targetObject, startPos, endPos, Time.time, duration));
+            return true;
+        }
+        else if (TweenExists(targetObject))// if false
+        {
+            Debug.Log("inside AddTween() first if");
+            return false;
+        }
+        else
+        {
+            Debug.Log("inside AddTween() second if");
+            activeTweens.Add(new Tween(targetObject, startPos, endPos, Time.time, duration));
+            return true;
+        }
+    
+    }
+
+    public bool TweenExists(Transform target)
+    {
+        Debug.Log("inside TweenExists()");
+        for (int i = 0; i < activeTweens.Count; i++)
+        {
+            Debug.Log("inside TweenExists() for loop");
+            if (target == activeTweens[i].Target)
+            {
+                Debug.Log("inside TweenExists() if loop");
+                return true;
+            }
+        }
+        return false;
     }
 
 }
